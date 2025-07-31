1c651055a3dbb9636134253c186ceaeb46d5a4b6
Niel-D22 <danielwarouw01@gmail.com>
2025-07-31 22:32:52 +0800

    complate:fitur dark-mode

--- FE/src/components/Navbar/Navbar.jsx
+++ FE/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Navbar = ({DarkMode, toggleDarkMode}) => {
+const Navbar = ({ DarkMode, toggleDarkMode }) => {
   return (
     <nav className="bg-white dark:text-white  dark:hover:text-blue-400  dark:bg-gray-800 text-gray-800 shadow-md  py-4 flex items-center justify-between px-20">
       {/* Logo atau Judul */}
@@ -22,10 +22,53 @@ const Navbar = ({DarkMode, toggleDarkMode}) => {
         <button className="dark:bg-blue-400 dark:text-gray-900 dark:hover:bg-blue-500 rounded-2xl hover:text-white-300 bg-blue-500 hover:bg-blue-600 text-white w-22 h-10">
           Login
         </button>
-         <button onClick={toggleDarkMode} className='px-4 py-2 bg-blue-500 text-white rounded-2xl
-       transition-colors dark:bg-blue-400 dark:text-gray-900 dark:hover:bg-blue-500 hover:bg-blue-600 '
-      >darkmode</button>
 
+ 
+      {/* button darkmode */}
+        <button
+          id="theme-toggle"
+          title="Toggle light & dark"
+          aria-label={DarkMode ? "dark" : "light"}
+          aria-live="polite"
+          onClick={toggleDarkMode}
+          className={`w-12 h-12  border-gray-200 flex justify-center items-center rounded-full 
+    transition-all duration-500 shadow-inner border 
+    ${DarkMode ? "bg-gray-800 border-gray-700" : "bg-white border-gray-200"}`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            className={`transition-transform duration-500 ease-in-out ${
+              DarkMode ? "text-white rotate-[25deg]" : "text-gray-500 rotate-0"
+            }`}
+          >
+            {DarkMode ? (
+              // bulan
+         
+              <path
+                fill="currentColor"
+                d="m20 12.79A9 9 0 0 1 11.21 3 7 7 0 1 0 21  12.79z"
+                transform="translate(-4, -0) scale(1.2)"
+              />
+            ) : (
+              // matahari
+              <g stroke="currentColor" strokeWidth="2">
+                <circle cx="12" cy="12" r="5" fill="currentColor" />
+                <line x1="12" y1="1" x2="12" y2="3" />
+                <line x1="12" y1="21" x2="12" y2="23" />
+                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                <line x1="1" y1="12" x2="3" y2="12" />
+                <line x1="21" y1="12" x2="23" y2="12" />
+                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+              </g>
+            )}
+          </svg>
+        </button>
       </div>
     </nav>
   );
