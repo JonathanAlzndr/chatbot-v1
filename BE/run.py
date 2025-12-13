53c8725f83448745e60b43ac3074aff3f838b10f
Jonathan K <jkamagi41@gmail.com>
2025-12-13 19:34:02 +0800

    db: migrations

--- BE/run.py
+++ BE/run.py
@@ -1,21 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_migrate import Migrate
 from app.routes.student_routes import student_bp
 from app.routes.admin_routes import admin_bp
-from .app.utils.config import Config
-from .app.utils.extensions import bcrypt, db, jwt, migrate
-from flask import jsonify
+from app.utils.config import Config
+from app.utils.extensions import bcrypt, db, jwt, migrate
+from app.models.admin_model import Admin
+from app.models.history_model import History
+from app.models.student_model import Student
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    
     CORS(app)
-
     bcrypt.init_app(app)
     db.init_app(app)
     jwt.init_app(app)
-    migrate.init_app(app, db)
-
+    migrate.init_app(app, db) 
 
     @jwt.unauthorized_loader
     def unauthorized_callback(error):
@@ -32,12 +35,9 @@ def create_app():
     app.register_blueprint(student_bp)
     app.register_blueprint(admin_bp)
 
-    with app.app_context():
-        db.create_all()
-
-
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
